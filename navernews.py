28884f9adb81eb392134affeb23586f14be7b911
junho <kesu9251@naver.com>
2020-01-21 23:12:49 +0900

    Add Find the most mentioned word
    Signed-off-by: junho <kesu9251@naver.com>

--- navernews.py
+++ navernews.py
@@ -22,27 +22,27 @@ class NaverNewsCrawling:
 
     SEARCH_FROM_YEAR = 2020
     SEARCH_FROM_MONTH = 1
-    SEARCH_FROM_DAY = 18
+    SEARCH_FROM_DAY = 1
     WAIT_SECONDS = 60
-    PAGENUM = 5
+    PAGENUM = 50
 
     RELATEDVECTORFILE = "./word2vec/related_word2vec.model"
 
-
+    STOCKFOLDER = "증권"
     # 259금융, 258증권, 261산업재계, 771중기벤처, 260부동산, 262글로벌경제 310생활경제, 263겨에일반
     # 264청와대, 265국회, 267국방외교,
     #cateGory = [258, 259, 260, 261, 262, 263, 264, 265, 310, 771]
-    cateGory = [258,259,260]
+    cateGory = [258,259]
     #중복 체크를 위한 사전
     last_news = dict()
 
     def start_class(self):
 
-        #self.CrawlNaverNews()
+        self.CrawlNaverNews()
         #self.ContinueGettingNews()
         #self.MakeKomoranFile()
-        self.MakeRelatedVector("금융/20200118_komoran.txt")
-        self.SeekRealtedWords()
+        #self.MakeRelatedVector("금융/20200118_komoran.txt")
+        #self.SeekRealtedWords()
 
     #실시간으로 업데이트 되는 뉴스를 parsing 한다.
     def ContinueGettingNews(self):
@@ -66,7 +66,7 @@ class NaverNewsCrawling:
                 except Exception as e:
                     print(e)
                     pass
-            time.sleep(WAIT_SECONDS)
+            time.sleep(self.WAIT_SECONDS)
 
 
     #특정날짜부터 현재날짜까지의 뉴스를 찾는다.
@@ -79,13 +79,20 @@ class NaverNewsCrawling:
         for c in self.cateGory:
             for t in dateTime:
                 for p in range(1, pageNum, 1):
+                    i=0
                     url_info = "https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&sid2={0}&sid1=100&date={1}&page={2}".format(c,t,p)
+                    print(url_info)
                     try:
                         news_url_list = self.ParseNewsURL(url_info)
                         for news_url in news_url_list :
                             news_class = self.GetNewsContent(news_url)
-                            print(news_class)
+                            if self.CheckLastNews(news_class, i) == True:
+                                i=-1
+                                break
                             WriteInfoToCsv(news_class, self.classCategory)
+                            i +=1
+                        if i == -1:
+                            break
                     except Exception as ex:
                         print(ex)
                         pass
@@ -119,6 +126,7 @@ class NaverNewsCrawling:
 
         news_class = dict()
         response = requests.get(url_info)
+        time.sleep(0.1)
         html = response.text
         soup = BeautifulSoup(html, 'html.parser')
 
@@ -206,6 +214,52 @@ class NaverNewsCrawling:
         except Exception as e:
             print(e)
 
+    #가장많이 언급된 단어를 찾는다.
+    #파일 리스트를 한꺼번에 받아서 거기서 읽어서 처리한다.
+    #input: file list(file contents is distinguished by ',' '\n')
+    #output : descending frequent word dictonary
+    def FindFrequentWords(self, file_list, num=10) :
+        word_list = list()
+        word_dict = list()
+
+        #파일 읽어서 리스트로 정리
+        #속도를 개선할 방법 필요
+        for file in file_list :
+            f = open(file)
+            data = f.read()
+            for word in re.split('\W+', data) :
+                if word in word_dict :
+                    word_dict[word] +=1
+                else :
+                    word_dict[word] = 1
+
+        word_list = sorted(word_dict.items(), key=lambda x:x[1], reverse=True)
+        return word_list[:num+1]
+
+    #해당 단어가 있는 파일 이름을 찾는다.
+    #input: word, file_list
+    #output: file_list
+    def FindDataRelatedWord(self, keyword, file_list) :
+        date_list = list()
+
+        for file in file_list :
+            f = open(file)
+            data = f.read()
+            if keyword in data:
+                date_list.append(file)
+            f.close()
+
+        return date_list
+
+    #해당 키워드와 연관된 주식들을 찾는다.
+    #input: keyword
+    #output: stock_list
+    def FindRelatedStock(self, keyword) :
+        files = glob.glob("*/????????.txt", recursive=True)
+        date_file_list = self.FindDateRelatedWord(keyword, files)
+        stock_file_list = glob.glob(self.STOCKFOLDER+"/????????_komoran.txt", recursive=True)
+        keyword_list = self.FindFrequentWords(stock_file_list, 10)
+        return keyword_list
 
 naver =NaverNewsCrawling()
 naver.start_class()
